6fb3e96c7ad79d6b22b2f5f8a9fcc882a7edf876
Luana Leite <luanaleites123@gmail.com>
2024-03-19 18:31:30 -0300

    exercicio de lógica

---
+++ NumerosCrescente/Program.cs
@@ -0,0 +1,65 @@
+﻿#region 
+//List<int> numeros = new List<int>() { 40, 3, 32, 5, 7, 2, 17, 1, 9, 19, 12, 8 };
+
+//numeros.Sort();
+
+//foreach (int item in numeros)
+//{
+//    Console.WriteLine(item);
+
+//}
+
+//numeros.Reverse();
+//foreach (int item in numeros)
+//{
+//    Console.WriteLine(item);
+//}
+#endregion
+
+int os = 0;
+int a = 0;
+int i = 0;
+int der = 0;
+int ys = 0;
+int an = 0;
+List<string> nomes = new List<string>() { "gustavo", "luana", "isadora", "rosane", "rosemeri", "chrystian" };
+List<string> palavras = new List<string>() { "os", "a", "i", "der", "ys", "an" };
+
+foreach (string item in nomes)
+{
+    if (item.Contains("os"))
+    {
+        os++;
+    }
+
+    if (item.Contains("a"))
+    {
+        a++;
+    }
+
+    if (item.Contains("i"))
+    {
+        i++;
+    }
+
+    if (item.Contains("der"))
+    {
+        der++;
+    }
+
+    if (item.Contains("ys"))
+    {
+        ys++;
+    }
+
+    if (item.Contains("an"))
+    {
+        an++;
+    }
+}
+
+Console.WriteLine($"E aqui está a contagem: {os}, {a}, {i}, {der}, {ys}, {an}");
+
+
+
+
